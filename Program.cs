9ea82e3ce67628763a96780472e5a3d1649bda19
Aliice77 <69522330+Aliice77@users.noreply.github.com>
2022-05-23 12:03:18 +0430

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace session1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a;
+            int b=0;
+            Console.WriteLine("enter a number");
+            a=Convert.ToInt32(Console.ReadLine());
+            for (int i=1;i<=a;i++)
+            {
+                if(i%2!=0)
+                {
+                    b=b+i;
+                }
+            }
+            Console.WriteLine(b);
+          
+        }
+    }
+    
+}
